766513cb9dc19588da621b3d553854d9fa4731c5
BenDMaryland <ben.d.maryland@gmail.com>
2021-11-23 09:43:37 -0500

    commit tree 34de2b5b98af4ff901361a247feca9f18eca6214

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,7 @@
 class PostsController < ApplicationController
 
+  rescue_from ActiveRecord::RecordInvalid, with: :render_unprocessable_entity_response
+
   def show
     post = Post.find(params[:id])
     
@@ -9,7 +11,7 @@ class PostsController < ApplicationController
   def update
     post = Post.find(params[:id])
 
-    post.update(post_params)
+    post.update!(post_params)
 
     render json: post
   end
@@ -19,5 +21,41 @@ class PostsController < ApplicationController
   def post_params
     params.permit(:category, :content, :title)
   end
+  
+  def render_unprocessable_entity_response(invalid)
+    render json: {errors: invalid.record.errors}, status: :unprocessable_entity
+  end
 
 end
+
+
+# rescue_from ActiveRecord::RecordNotFound, with :render_not_found
+# rescue_from ActiveRecord::RecordInvlaid, with :record_invlaid
+#     # validates :
+
+#     def create
+      
+#         author = Author.create!(author_params)
+#         render json: author, status: :created
+#       end
+
+
+
+
+#     private
+
+
+
+# def record_invlaid
+#     render json: { errors: invalid.record.errors }, status: :unprocessable_entity
+# end
+
+
+
+
+
+#    def render_not_found
+
+
+#     render json: {error: "eroor"} status: not_found
+#    end
